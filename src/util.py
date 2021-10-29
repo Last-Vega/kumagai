6b8e3d922294cdfe95358098d444bfea3974e877
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-10-29 15:40:21 +0900

    Create util.py

---
+++ src/util.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from sklearn.metrics import roc_auc_score, average_precision_score
+import scipy.sparse as sp
+import numpy as np
+
+from preprocessing import *
+
+from args import *
+from model import VGAE
+from model import GAE
+import pandas as pd
+
+
+def prepare_adj_for_training(adj):
+    # Store original adjacency matrix (without diagonal entries) for later
+    adj_orig = adj
+    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+    adj_orig.eliminate_zeros()
+
+    adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
+    adj = adj_train
+
+    # Some preprocessing
+    adj_norm = preprocess_graph(adj)
+
+    num_nodes = adj.shape[0]
+
+    # Create Model
+    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
+    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
+
+    adj_label = adj_train + sp.eye(adj_train.shape[0])
+    adj_label = sparse_to_tuple(adj_label)
+
+    adj_norm = torch.sparse.FloatTensor(torch.LongTensor(adj_norm[0].T),
+                                torch.FloatTensor(adj_norm[1]),
+                                torch.Size(adj_norm[2]))
+    adj_label = torch.sparse.FloatTensor(torch.LongTensor(adj_label[0].T),
+                                torch.FloatTensor(adj_label[1]),
+                                torch.Size(adj_label[2]))
+
+    weight_mask = adj_label.to_dense().view(-1) == 1
+    weight_tensor = torch.ones(weight_mask.size(0))
+    weight_tensor[weight_mask] = pos_weight
+    return weight_tensor, adj_norm, norm, adj_label, adj_orig, test_edges, test_edges_false
+
+def prepare_features_for_training(features):
+    features = sparse_to_tuple(features.tocoo())
+    num_features = features[2][1]
+    features_nonzero = features[1].shape[0]
+    features = torch.sparse.FloatTensor(torch.LongTensor(features[0].T),
+                                torch.FloatTensor(features[1]),
+                                torch.Size(features[2]))
+    
+    return features
+
+def model_init(adj_norm, input_dims):
+    model = GAE(adj_norm, input_dims)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    return model, optimizer
