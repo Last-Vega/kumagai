71698e5a955d6601c9b1555426304110b15d21b0
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-29 17:27:11 +0900

    Update input_data.py

--- src/input_data.py
+++ src/input_data.py
@@ -1,18 +1,24 @@
+from networkx.classes.graph import Graph
 import numpy as np
 import networkx as nx
 import pickle
 from scipy.sparse import csr_matrix, lil_matrix
 
-def make_adj(f_name:str) -> np.ndarray:
+def company_adj(f_name:str) -> csr_matrix:
     with open(f_name, 'rb') as rb:
-        g:any = pickle.load(rb)
+        g:Graph = pickle.load(rb)
     adj:np.ndarray = nx.to_numpy_array(g, dtype=int)
+    adj:csr_matrix = csr_matrix(adj)
     return adj
 
-
+def company_term_biadj(f_name:str) -> csr_matrix:
+    with open(f_name, 'rb') as rb:
+        bi_adj:csr_matrix = pickle.load(rb)
+    return bi_adj
 
 f_name:str = '../vars/c_c.graph'
-adj:np.ndarray = make_adj(f_name)
-adj:csr_matrix = csr_matrix(adj)
-print(type(adj))
-# print(adj)
+adj:csr_matrix = company_adj(f_name)
+
+f_name:str = '../vars/c_t.biadj'
+bi_adj:csr_matrix = company_term_biadj(f_name)
+
