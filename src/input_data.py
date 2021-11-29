1b34fd1df70970c3c9bd3b1a1a90a724f9b4250c
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-29 19:19:53 +0900

    Update input_data.py

--- src/input_data.py
+++ src/input_data.py
@@ -16,9 +16,19 @@ def company_term_biadj(f_name:str) -> csr_matrix:
         bi_adj:csr_matrix = pickle.load(rb)
     return bi_adj
 
+def createFeatures(adj:csr_matrix) -> lil_matrix:
+    n = adj.shape[0]
+
+    features = np.zeros((n, n))
+    for i in range(n):
+        features[i][i] = 1
+    features = lil_matrix(features)
+    return features
+
 f_name:str = '../vars/c_c.graph'
 adj:csr_matrix = company_adj(f_name)
 
+features:lil_matrix = createFeatures(adj)
+
 f_name:str = '../vars/c_t.biadj'
 bi_adj:csr_matrix = company_term_biadj(f_name)
-
