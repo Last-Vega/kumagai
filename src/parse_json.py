c01d6cf44e338e85c8c33a999bbc5f2658116334
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-29 11:38:16 +0900

    Update parse_json.py

--- src/parse_json.py
+++ src/parse_json.py
@@ -1,17 +1,3 @@
-# def create_node(G:any, json_data:dict) -> any:
-#     company_list:list = []
-#     for itr in range(len(json_data)):
-#         itr = str(itr)
-#         if 'createdBy' in json_data[itr].keys():
-            
-#             # data['createdBy'] = re.sub(re_obj, '', data['createdBy'])
-#             for company in set(json_data[itr]['createdBy']):
-#                 # if not company in company_list:
-#                 #     G.add_node(company)
-#                 #     company_list.append(company)
-
-#     return G
-
 def changeType(json_data:dict) -> dict:
     company_pattern = r"((?:[^\\']+|\\.){3})"
     term_pattern = r"[a-z]"
@@ -25,9 +11,12 @@ def changeType(json_data:dict) -> dict:
         else:
             json_data[itr]['f_term'] = ['']
         
-        if type(f_term) is str:
+        if type(createdBy) is str:
             comp_list = re.findall(company_pattern, createdBy)
             json_data[itr]['createdBy'] = comp_list
+        # else:
+        #     print(json_data[itr]['createdBy'])
+        #     print(type(json_data[itr]['createdBy']))
         
         if type(term) is str:
             if regex.search(term_pattern, term):
@@ -46,7 +35,6 @@ def changeType(json_data:dict) -> dict:
                 dic = ast.literal_eval(term)
             else:
                 dic = ast.literal_eval(term)
-            print(itr)
             json_data[itr]['term'] = dic
         
     
@@ -55,10 +43,6 @@ def changeType(json_data:dict) -> dict:
 import json
 import re
 import regex
-import networkx as nx
-import numpy as np
-from scipy.sparse import csr_matrix, lil_matrix
-import matplotlib.pyplot as plt
 import ast
 
 f_name = '../data/patent.json'
@@ -70,9 +54,3 @@ json_data = changeType(json_data)
 with open('../data/patent_parsed.json', 'w') as f:
     result = json.dump(json_data, f, indent=2, ensure_ascii=False)
 
-
-    
-# graph:any = nx.Graph()
-# G:any = create_node(graph, json_data)
-# print(len(G.nodes))
-# print(G.nodes)
