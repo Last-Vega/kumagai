ef249fee217eb22f3424dcf577f3c7d4cc1dc182
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-29 15:05:44 +0900

    Update createGraph.py

--- src/createGraph.py
+++ src/createGraph.py
@@ -19,14 +19,14 @@ def makeCompanyRelation(patent_info:dict, json_data:dict, G:any, itr:str) -> Non
     for company in patent_info['createdBy']:
         print(itr)
         for i in range(len(json_data)):
-            if int(itr) > i:
+            if int(itr) >= i:
                 continue
             else:
                 i = str(i)
                 if 'createdBy' in json_data[i].keys():
                     comp_company_list = json_data[i]['createdBy']
                     for comp_company in comp_company_list:
-                        if comp_company != company:
+                        if comp_company == company:
                             G.add_edge(company, comp_company)
     return 
 
@@ -39,21 +39,33 @@ def buildCompanyGraph(G:any, json_data:dict) -> any:
 
 
 import json
-import re
-import regex
 import networkx as nx
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
 import itertools
 
+# f_name = '../data/sample.json'
 f_name = '../data/patent_parsed.json'
+
 json_open = open(f_name, 'r')
 json_data = json.load(json_open)
     
 graph:any = nx.Graph()
 G:any = create_node(graph, json_data)
 G = buildCompanyGraph(G, json_data)
-print(len(G.nodes()))
 adj:np.ndarray = nx.to_numpy_array(G, dtype=int)
 print(adj)
+
+
+import pickle
+with open('../vars/c_c.graph', 'wb') as bf:
+    pickle.dump(G, bf)
+
+with open('../vars/c_cAdj.adj', 'wb') as bf2:
+    pickle.dump(adj, bf2)
 # print(G.nodes())
+
+# with open('../vars/c_c.graph', 'rb') as rb:
+#   dumpedG = pickle.load(rb)
+
+# print(dumpedG.nodes())
