6cabcae87bf08fbb7bb262b513fcf55227ffe7b4
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-29 15:55:35 +0900

    Update createBigraph.py

--- src/createBigraph.py
+++ src/createBigraph.py
@@ -1,12 +1,23 @@
 import json
-from typing_extensions import Literal
 import networkx as nx
 from networkx.classes.graph import Graph
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
 import itertools
 
-def create_node(G:any, json_data:dict) -> Graph:
+def create_node(G:Graph, json_data:dict) -> Graph:
+    company_list:list = []
+    for itr in range(len(json_data)):
+        itr = str(itr)
+        if 'createdBy' in json_data[itr].keys():
+            for company in json_data[itr]['createdBy']:
+                if not company in company_list:
+                    G.add_node(company)
+                    company_list.append(company)
+
+    return G
+
+def create_Term_node(G:any, json_data:dict) -> Graph:
     term_list:list = []
     for itr in range(len(json_data)):
         itr = str(itr)
@@ -25,6 +36,7 @@ json_data:dict = json.load(json_open)
 
 graph:Graph = nx.Graph()
 G:Graph = create_node(graph, json_data)
+G:Graph = create_Term_node(graph, json_data)
 
-print(G.nodes())
+# print(G.nodes())
 print(len(G.nodes()))
