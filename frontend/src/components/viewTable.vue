94b43f99cb9bc07fd52b0021f1227745502f2438
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-12-24 00:59:52 +0900

    Update viewTable.vue

--- frontend/src/components/viewTable.vue
+++ frontend/src/components/viewTable.vue
@@ -12,63 +12,77 @@
       </v-col>
     </v-row>
   </v-form>
-  <v-simple-table>
-    <template v-slot:default>
-      <caption>
-        検索された単語の座標
-      </caption>
-      <thead>
-        <tr>
-          <th class="text-left">X</th>
-          <th class="text-left">Y</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>000</td>
-          <td>111</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
-  <v-simple-table>
-    <template v-slot:default>
-      <caption>
-        近くの企業
-      </caption>
-      <thead>
-        <tr>
-          <th class="text-left">Company</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
-  <v-simple-table>
-    <template v-slot:default>
-      <caption>
-        近くの単語
-      </caption>
-      <thead>
-        <tr>
-          <th class="text-left">Term</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <div v-if="showFlag===true">
+    <v-simple-table>
+      <template v-slot:default>
+        <caption>
+          検索された単語の座標
+        </caption>
+        <thead>
+          <tr>
+            <th class="text-left">X</th>
+            <th class="text-left">Y</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{queryX}}</td>
+            <td>{{queryY}}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-simple-table>
+      <template v-slot:default>
+        <caption>
+          近くの企業
+        </caption>
+        <thead>
+          <tr>
+            <th class="text-left">Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(company, index) in closeCompany"
+            :key="index"
+            >
+            <td>{{company}}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-simple-table>
+      <template v-slot:default>
+        <caption>
+          近くの単語
+        </caption>
+        <thead>
+          <tr>
+            <th class="text-left">Term</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(term, index) in closeTerm"
+            :key="index"
+            >
+            <td>{{term}}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+  <div v-else>
+    <p>ヒットしませんでした</p>
+  </div>
 </div>
 </template>
 
 <script>
+import companyInfo from '@/assets/latentC1223.json'
+import termInfo from '@/assets/latentT1223.json'
+
 export default {
   name: 'ViewTabel',
   props: {
@@ -83,23 +97,93 @@ export default {
   },
   data () {
     return {
-      query: ''
+      query: '',
+      closeCompany: [],
+      closeTerm: [],
+      showFlag: false,
+      companyInfo: '',
+      termInfo: '',
+      queryX: '',
+      queryY: ''
     }
   },
   methods: {
     submit () {
+      this.closeCompany = []
+      this.closeTerm = []
       let filterIndex = -1
+      let flag = -1
       if (this.companyName.includes(this.query)) {
         filterIndex = this.companyName.indexOf(this.query)
+        flag = 0
       } else if (this.termName.includes(this.query)) {
         filterIndex = this.termName.indexOf(this.query)
+        flag = 1
       } else {
         filterIndex = -1
+        flag = -1
+        this.showFlag = false
       }
-      console.log(filterIndex)
+      if (filterIndex !== -1) {
+        this.showFlag = true
+        if (flag === 0) {
+          this.queryX = this.companyInfo[filterIndex].x
+          this.queryY = this.companyInfo[filterIndex].y
+          this.calcCompanyDistance(filterIndex, this.companyInfo[filterIndex].x, this.companyInfo[filterIndex].y, false)
+          this.calcTermDistance(filterIndex, this.companyInfo[filterIndex].x, this.companyInfo[filterIndex].y, true)
+        } else if (flag === 1) {
+          this.queryX = this.termInfo[filterIndex].x
+          this.queryY = this.termInfo[filterIndex].y
+          this.calcCompanyDistance(filterIndex, this.termInfo[filterIndex].x, this.termInfo[filterIndex].y, true)
+          this.calcTermDistance(filterIndex, this.companyInfo[filterIndex].x, this.companyInfo[filterIndex].y, false)
+        }
+      }
+    },
+    calcCompanyDistance (index, x, y, flag) {
+      const distanceList = []
+      this.companyInfo.forEach((value) => {
+        const tempObj = {}
+        const distance = Math.sqrt((value.x - x) ** 2 + (value.y - y) ** 2)
+        tempObj.key = value.company
+        tempObj.value = distance
+        distanceList.push(tempObj)
+      })
+      distanceList.sort((a, b) => a.value - b.value)
+      if (flag === true) {
+        for (let i = 0; i < 5; i++) {
+          this.closeCompany.push(distanceList[i].key)
+        }
+      } else {
+        for (let i = 1; i < 6; i++) {
+          this.closeCompany.push(distanceList[i].key)
+        }
+      }
+    },
+    calcTermDistance (index, x, y, flag) {
+      const distanceList = []
+      this.termInfo.forEach((value) => {
+        const tempObj = {}
+        const distance = Math.sqrt((value.x - x) ** 2 + (value.y - y) ** 2)
+        tempObj.key = value.term
+        tempObj.value = distance
+        distanceList.push(tempObj)
+      })
+      distanceList.sort((a, b) => a.value - b.value)
+      if (flag === true) {
+        for (let i = 0; i < 5; i++) {
+          this.closeTerm.push(distanceList[i].key)
+        }
+      } else {
+        for (let i = 1; i < 6; i++) {
+          this.closeTerm.push(distanceList[i].key)
+        }
+      }
     }
+  },
+  created () {
+    this.companyInfo = companyInfo.key
+    this.termInfo = termInfo.key
   }
-
 }
 </script>
 
